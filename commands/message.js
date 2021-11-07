14d5bbed6c46db8517259f89c194c6915054c9d3
raffii3424235 <raffifariz12@gmail.com>
2021-11-07 14:15:35 +0700

    update link website

--- commands/message.js
+++ commands/message.js
@@ -8,5 +8,17 @@ module.exports = {
         channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
         member: [],
     },
-    aliases: ["web", ""]
+    aliases: ["web", "websites"],
+
+    run: async (client, message) => {
+        const embed = new MessageEmbed();
+        embed.setColor(client.botconfig.EmbedColor);
+        embed.setTitle(`Website dari ${client.user.username}`);
+        embed.addFields(
+            {
+                name: `[Hit Me!!!](https://carakubeiya.unaux.com/)`,
+                value: `https://cdn.discordapp.com/attachments/865743194414383134/906802671669903370/Screenshot_from_2021-11-07_14-07-35.png`,
+            },
+        );
+    },
 };
